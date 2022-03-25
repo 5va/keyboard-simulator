da943ec5ecfdfc0d231015f40355967a08d783a1
5va <paul.travis.knowles@gmail.com>
2022-03-25 16:11:14 -0500

    Update front-end

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', () =>{
 
     const keys = document.querySelectorAll(".container #key");
+    const container=document.getElementsByClassName("taskbar")
     for (let i = 0; i < keys.length; i++) {
         keys[i].onclick = ({ target }) => {
-            alert(target.innerText)
+            let letter = document.createElement("div");
+            letter.innerText=target.innerText
+            letter.setAttribute("id","square")
+
+            container[0].appendChild(letter);
         }};
 })
